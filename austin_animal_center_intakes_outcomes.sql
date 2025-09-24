32b90e275546b87ee43b4b0b8ae13d9fcdb75539
Kirsten Bjornson <79984576+kbjornson@users.noreply.github.com>
2025-09-24 15:46:59 -0500

    Update austin_animal_center_intakes_outcomes.sql

--- austin_animal_center_intakes_outcomes.sql
+++ austin_animal_center_intakes_outcomes.sql
@@ -7,34 +7,6 @@ Purpose: Combine animal intakes and animal outcomes datasets to see the full pic
 		 of an animal's stay and outcome after being taken to the shelter
 
 ******************************************************/
-DROP TABLE IF EXISTS AnimalIntake;
-
-CREATE TABLE AnimalIntake (
-    animal_id VARCHAR(50),
-    name VARCHAR(100),
-	datetime DATETIME,
-	monthyear VARCHAR(20),
-	intake_type VARCHAR(50),
-    intake_condition VARCHAR(50),
-    animal_type VARCHAR(50),
-    sex_upon_intake VARCHAR(50),
-    age_upon_intake VARCHAR(50),
-	breed VARCHAR(100),
-    color VARCHAR(100)
-);
-
-BULK INSERT AnimalIntake
-FROM 'C:/Users/bjorn/Documents/Austin_Animal_Center_Project/Austin_Animal_Center_Intakes_Cleaned.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    TABLOCK
-);
-
--- drop intakes table and drop columns in python that I'm not using and reimport 
--- check distinct animal type
-
 with intake_seq as (
 	select *,
 		row_number() over (partition by animal_id order by cast(datetime as date)) as seq
@@ -79,9 +51,3 @@ where o.date >= cast(i.datetime as date)
 
 --order by i.animal_id, cast(i.datetime as date)
 ;
-
-select animal_id, cast(datetime as date) from AnimalShelterDB.dbo.AnimalIntake
-where animal_id = 'A660279';
-
-select animal_id, date from AnimalShelterDB.dbo.AnimalOutcome
-where animal_id = 'A660279';
